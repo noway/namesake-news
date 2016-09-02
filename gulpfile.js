dc96dbecac755bca12e46a6c39a0124d298bb9fd
Way, No <noway@2ch.hk>
2016-09-02 20:52:31 +1200

    ts in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,40 @@
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
+var merge = require('merge2');
  
-gulp.task('default', function () {
-    return gulp.src('routes/**/*.ts')
-        .pipe(ts({
-            noImplicitAny: true,
-            out: 'output.js'
-        }))
-        .pipe(gulp.dest('built/local'));
+var tsProject = ts.createProject({
+    declaration: true,
+    noExternalResolve: false
 });
+var tsProjectMain = ts.createProject({
+    declaration: true,
+    noExternalResolve: false
+});
+ 
+gulp.task('scripts', function() {
+    var tsResult = gulp.src('routes/*.ts')
+        .pipe(ts(tsProject));
+ 
+    return merge([ // Merge the two output streams, so this task is finished when the IO of both operations are done. 
+        tsResult.dts.pipe(gulp.dest('routes/defs')),
+        tsResult.js.pipe(gulp.dest('routes/'))
+    ]);
+});
+gulp.task('main', function() {
+    var tsResult = gulp.src('app.ts')
+        .pipe(ts(tsProjectMain));
+ 
+    return merge([ // Merge the two output streams, so this task is finished when the IO of both operations are done. 
+        tsResult.dts.pipe(gulp.dest('./')),
+        tsResult.js.pipe(gulp.dest('./'))
+    ]);
+});
+ 
+gulp.task('watch', ['scripts'], function() {
+    gulp.watch('routes/*.ts', ['scripts']);
+});
+gulp.task('watch', ['main'], function() {
+    gulp.watch('routes/*.ts', ['main']);
+});
+
+gulp.task('default', ['scripts','main']);
