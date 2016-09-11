36bc35375d89ff92788398a3531347dbbe9621bb
Way, No <noway@2ch.hk>
2016-09-11 21:31:02 +1200

    typescript errors

--- public/index.ts
+++ public/index.ts
@@ -3,12 +3,9 @@
 $('document').ready(function() {
     $('.login-button').click(function(e){
         FB.login(function(response) {
-            if (response.authResponse) {
+            if (response['authResponse']) {
                 console.log('Welcome!  Fetching your information.... ');
-                FB.api('/me', function(response) {
-                    window.location.href = '/news';
-                    console.log('Good to see you, ' + response.name + '.');
-                });
+                window.location.href = '/news';
             } else {
                 console.log('User cancelled login or did not fully authorize.');
             }
